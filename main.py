f5c04e2a2ddcd061e0b1f0c4642d3f500e681b93
dae <notloneth@gmail.com>
2022-03-01 18:28:10 +0700

    Rewrite

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ import time
 
 time.sleep(5)
 
-f = open("text", "r")
+try:
+    a = str(input("Word to spam: "))
+    b = int(input("How much: "))
 
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+    for i in range(b):
+        pyautogui.typewrite(a)
+        pyautogui.press("enter")
+
+except:
+    print("error")
+
+# time.sleep(5)
+
+# f = open("text", "r")
+
+# for word in f:
+#     pyautogui.typewrite(word)
+#     pyautogui.press("enter")
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+title Spamming Tool
+python -B main.py
+@pause
